777ffb973de363a0e5fd153eafa7a22b00b0065c
Tyler-Eaton <tymeat39@gmail.com>
2024-05-31 17:32:11 -0400

    fixed icons and changed profile pic

--- blinds.js
+++ blinds.js
@@ -15,7 +15,7 @@ function VenetianBlinds(defaults)
   var img2 = defaults.imgSrc2;
   var placeholder = document.getElementsByClassName(defaults.containerName)[0];
   var directionX, directionY;
-  
+
   var column, blind, blindImg;
   var bgImg, rot;
   var colL;
@@ -23,13 +23,13 @@ function VenetianBlinds(defaults)
   for (var i=0; i < cols; i++)
     {
       colL = ((colW + margin) * i);
-      
+
       column = document.createElement('div');
       column.className = "column";
       column.style.width = colW + "px";
       column.style.left = colL + "px";
-      placeholder.appendChild(column); 
-      
+      placeholder.appendChild(column);
+
       for (var j=0; j<4; j++)
         {
           blind = document.createElement('div');
@@ -37,7 +37,7 @@ function VenetianBlinds(defaults)
           blind.style.width = colW + "px";
           blindImg = document.createElement('div');
           blindImg.className = "blindImg";
-          
+
           switch (j){
              case 0:
                 TweenMax.set(blind, {rotationY: "0"});
@@ -47,10 +47,10 @@ function VenetianBlinds(defaults)
                 TweenMax.set(blind, {rotationY: "90"});
                 bgImg = img2;
                 break;
-             case 2: 
+             case 2:
                   TweenMax.set(blind, {rotationY: "180"});
                   bgImg = img1;
-                  break;              
+                  break;
               case 3:
                   TweenMax.set(blind, {rotationY: "270"});
                   bgImg = img2;
@@ -62,16 +62,16 @@ function VenetianBlinds(defaults)
 
           column.appendChild(blind);
           blind.appendChild(blindImg);
-          
+
           TweenMax.set(blind, { transformOrigin:"50% 50% " + -colW/2, transformStyle: "preserve-3d"});
         }
-      
+
       TweenMax.set(column, {transformStyle:"preserve-3d", transformPerspective:1000, rotationY:0});
-      
+
        column.addEventListener("mouseenter", function(event){
             var elem = event.currentTarget;
             var rotY = elem._gsTransform.rotationY;
-         
+
             if(directionX > 0){
               TweenMax.to(elem, 1, {rotationY:Math.floor(rotY/90)*90+90, transformOrigin:"50% 50% -" + colW/2, ease:Back.easeOut});
             }else{
